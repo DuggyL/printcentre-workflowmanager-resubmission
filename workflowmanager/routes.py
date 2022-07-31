a2caebbc486f3f9a50fb0d24b35e9c1f6bc873ff
Dustin Li <dustin.li1988@gmail.com>
2022-07-31 19:35:46 +0000

    Setting up add task functionality and page

--- workflowmanager/routes.py
+++ workflowmanager/routes.py
@@ -10,3 +10,27 @@ from workflowmanager import app, mongo
 def get_tasks():
     tasks = list(mongo.db.tasks.find())
     return render_template("tasks.html", tasks=tasks)
+
+@app.route("/add_task", methods=["GET", "POST"])
+def add_task():
+    if request.method == "POST":
+        is_urgent = "on" if request.form.get("urgent") else "off"
+        task = {
+            "job_title": request.form.get("job_title"),
+            "company_name": request.form.get("company_name"),
+            "job_description": request.form.get("job_description"),
+            "quantity": request.form.get("quantity"),
+            "materials": request.form.get("materials"),
+            "printing": request.form.get("printing"),
+            "finishing": request.form.get("finishing"),
+            "delivery": request.form.get("delivery"),
+            "delivery_date": request.form.get("delivery_date"),
+            "delivery_address": request.form.get("delivery_address"),
+            "urgent": is_urgent,
+            "created_by": session["user"]
+        }
+        mongo.db.tasks.insert_one(task)
+        flash("Task Successfully Added", category='success')
+        return redirect(url_for("get_tasks"))
+    customers = list(Customer.query.order_by(Customer.company_name).all())
+    return render_template("add_task.html", customers=customers)

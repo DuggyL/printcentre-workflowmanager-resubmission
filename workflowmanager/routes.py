27acf1fbc7c8f8e120090dbfdf62e2aaeb282885
Dustin Li <dustin.li1988@gmail.com>
2022-07-31 13:02:18 +0000

    Add base.html and tasks.html

--- workflowmanager/routes.py
+++ workflowmanager/routes.py
@@ -1,14 +1,11 @@
 from flask import (
     Flask, flash, render_template, redirect, request, session, url_for)
-#from flask_pymongo import PyMongo
-#from bson.objectid import ObjectId
-#from werkzeug.security import generate_password_hash, check_password_hash
-from workflowmanager import app #mongo
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
+from workflowmanager import app, mongo
 
 @app.route("/")
-def hello():
-    return "I hate this project!"
-
 @app.route("/get_tasks")
 def get_tasks():
     tasks = list(mongo.db.tasks.find())
